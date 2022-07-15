bd09f3a3afe67b6671f5f14aff55539acf02a16c
Philipp Krauss <philipp.krauss@codecentric.de>
2022-07-15 09:05:59 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const width = 10
+const height = 10
+const initField: Array<Array<boolean>> = []
+
+for (let currentRow = 0; currentRow < height; currentRow++) {
+    const row = []
+    for (let currentCol = 0; currentCol < width; currentCol++) {
+        row.push(Math.random() > 0.5)
+    }
+    initField.push(row)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let count = 0
+    const [field, setField] = useState(initField)
+    const renderedField = field.map((row) => {
+        const renderedRow = row.map((cell) => {
+            return <td>{cell?'X':'_'}</td>
+        })
+        return <tr>{renderedRow}</tr>
+    })
+    function handleTick() {
+        const deepCopy = JSON.parse(JSON.stringify(field))
+        deepCopy[0][0] = false
+        setField(deepCopy)
+    }
+    return (
+        <div className="App">
+            <h1>GOL</h1>
+            <table>{renderedField}</table>
+            <button onClick={handleTick}>Tick</button>
+        </div>
+    );
 }
 
 export default App;
